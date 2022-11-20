bafd85c9db54088b39ceaf0d0f97acb3d8eb56cc
Лука Руслан <lukasus22mail.ru@gmail.com>
2022-11-20 20:43:08 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace CourseWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewLexeme = new System.Windows.Forms.DataGridView();
+            this.ButtonOpenFile = new System.Windows.Forms.Button();
+            this.ButtonLexemeAnalys = new System.Windows.Forms.Button();
+            this.textBoxCode = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.dataGridViewKeywords = new System.Windows.Forms.DataGridView();
+            this.dataGridViewSeparators = new System.Windows.Forms.DataGridView();
+            this.dataGridViewVariables = new System.Windows.Forms.DataGridView();
+            this.dataGridViewLiterals = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTokens = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLexeme)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewKeywords)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSeparators)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewVariables)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLiterals)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTokens)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewLexeme
+            // 
+            this.dataGridViewLexeme.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewLexeme.Location = new System.Drawing.Point(290, 12);
+            this.dataGridViewLexeme.Name = "dataGridViewLexeme";
+            this.dataGridViewLexeme.RowHeadersVisible = false;
+            this.dataGridViewLexeme.Size = new System.Drawing.Size(252, 423);
+            this.dataGridViewLexeme.TabIndex = 0;
+            // 
+            // ButtonOpenFile
+            // 
+            this.ButtonOpenFile.Location = new System.Drawing.Point(12, 360);
+            this.ButtonOpenFile.Name = "ButtonOpenFile";
+            this.ButtonOpenFile.Size = new System.Drawing.Size(75, 48);
+            this.ButtonOpenFile.TabIndex = 1;
+            this.ButtonOpenFile.Text = "Открыть файл";
+            this.ButtonOpenFile.UseVisualStyleBackColor = true;
+            this.ButtonOpenFile.Click += new System.EventHandler(this.ButtonOpenFile_Click);
+            // 
+            // ButtonLexemeAnalys
+            // 
+            this.ButtonLexemeAnalys.Location = new System.Drawing.Point(159, 360);
+            this.ButtonLexemeAnalys.Name = "ButtonLexemeAnalys";
+            this.ButtonLexemeAnalys.Size = new System.Drawing.Size(82, 48);
+            this.ButtonLexemeAnalys.TabIndex = 2;
+            this.ButtonLexemeAnalys.Text = "Выполнить лексический анализ";
+            this.ButtonLexemeAnalys.UseVisualStyleBackColor = true;
+            this.ButtonLexemeAnalys.Click += new System.EventHandler(this.ButtonLexemeAnalys_Click);
+            // 
+            // textBoxCode
+            // 
+            this.textBoxCode.Location = new System.Drawing.Point(12, 12);
+            this.textBoxCode.Multiline = true;
+            this.textBoxCode.Name = "textBoxCode";
+            this.textBoxCode.Size = new System.Drawing.Size(229, 342);
+            this.textBoxCode.TabIndex = 3;
+            this.textBoxCode.Text = "Dim x as integer\r\nDim i, a as integer\r\nfor i=1 to 10\r\nx=x+i*5\r\nnext for";
+            // 
+            // dataGridViewKeywords
+            // 
+            this.dataGridViewKeywords.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewKeywords.Location = new System.Drawing.Point(584, 12);
+            this.dataGridViewKeywords.Name = "dataGridViewKeywords";
+            this.dataGridViewKeywords.RowHeadersVisible = false;
+            this.dataGridViewKeywords.Size = new System.Drawing.Size(189, 209);
+            this.dataGridViewKeywords.TabIndex = 4;
+            // 
+            // dataGridViewSeparators
+            // 
+            this.dataGridViewSeparators.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSeparators.Location = new System.Drawing.Point(801, 12);
+            this.dataGridViewSeparators.Name = "dataGridViewSeparators";
+            this.dataGridViewSeparators.RowHeadersVisible = false;
+            this.dataGridViewSeparators.Size = new System.Drawing.Size(189, 209);
+            this.dataGridViewSeparators.TabIndex = 5;
+            // 
+            // dataGridViewVariables
+            // 
+            this.dataGridViewVariables.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewVariables.Location = new System.Drawing.Point(584, 227);
+            this.dataGridViewVariables.Name = "dataGridViewVariables";
+            this.dataGridViewVariables.RowHeadersVisible = false;
+            this.dataGridViewVariables.Size = new System.Drawing.Size(189, 209);
+            this.dataGridViewVariables.TabIndex = 6;
+            // 
+            // dataGridViewLiterals
+            // 
+            this.dataGridViewLiterals.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewLiterals.Location = new System.Drawing.Point(801, 226);
+            this.dataGridViewLiterals.Name = "dataGridViewLiterals";
+            this.dataGridViewLiterals.RowHeadersVisible = false;
+            this.dataGridViewLiterals.Size = new System.Drawing.Size(189, 209);
+            this.dataGridViewLiterals.TabIndex = 7;
+            // 
+            // dataGridViewTokens
+            // 
+            this.dataGridViewTokens.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTokens.Location = new System.Drawing.Point(1009, 12);
+            this.dataGridViewTokens.Name = "dataGridViewTokens";
+            this.dataGridViewTokens.RowHeadersVisible = false;
+            this.dataGridViewTokens.Size = new System.Drawing.Size(252, 423);
+            this.dataGridViewTokens.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1273, 447);
+            this.Controls.Add(this.dataGridViewTokens);
+            this.Controls.Add(this.dataGridViewLiterals);
+            this.Controls.Add(this.dataGridViewVariables);
+            this.Controls.Add(this.dataGridViewSeparators);
+            this.Controls.Add(this.dataGridViewKeywords);
+            this.Controls.Add(this.textBoxCode);
+            this.Controls.Add(this.ButtonLexemeAnalys);
+            this.Controls.Add(this.ButtonOpenFile);
+            this.Controls.Add(this.dataGridViewLexeme);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLexeme)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewKeywords)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSeparators)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewVariables)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLiterals)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTokens)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewLexeme;
+        private System.Windows.Forms.Button ButtonOpenFile;
+        private System.Windows.Forms.Button ButtonLexemeAnalys;
+        private System.Windows.Forms.TextBox textBoxCode;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataGridView dataGridViewKeywords;
+        private System.Windows.Forms.DataGridView dataGridViewSeparators;
+        private System.Windows.Forms.DataGridView dataGridViewVariables;
+        private System.Windows.Forms.DataGridView dataGridViewLiterals;
+        private System.Windows.Forms.DataGridView dataGridViewTokens;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseWork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetupTables();
+        }
+
+        private void SetupTables()
+        {
+            dataGridViewLexeme.Columns.Add("Word", "Слово");
+            dataGridViewLexeme.Columns.Add("Lexeme", "Лексема");
+            dataGridViewLexeme.Columns[0].Width = dataGridViewLexeme.Width / 2;
+            dataGridViewLexeme.Columns[1].Width = dataGridViewLexeme.Width / 2;
+
+            dataGridViewKeywords.Columns.Add("Keywords", "Ключевые слова");
+            dataGridViewKeywords.Columns.Add("Value", "Значение");
+            dataGridViewKeywords.Columns[0].Width = dataGridViewKeywords.Width / 2;
+            dataGridViewKeywords.Columns[1].Width = dataGridViewKeywords.Width / 2;
+
+            dataGridViewSeparators.Columns.Add("Separators", "Разделители");
+            dataGridViewSeparators.Columns.Add("Value", "Значение");
+            dataGridViewSeparators.Columns[0].Width = dataGridViewSeparators.Width / 2;
+            dataGridViewSeparators.Columns[1].Width = dataGridViewSeparators.Width / 2;
+
+            dataGridViewVariables.Columns.Add("Variables", "Переменные");
+            dataGridViewVariables.Columns.Add("Value", "Значение");
+            dataGridViewVariables.Columns[0].Width = dataGridViewVariables.Width / 2;
+            dataGridViewVariables.Columns[1].Width = dataGridViewVariables.Width / 2;
+
+            dataGridViewLiterals.Columns.Add("Literals", "Литералы");
+            dataGridViewLiterals.Columns.Add("Value", "Значение");
+            dataGridViewLiterals.Columns[0].Width = dataGridViewLiterals.Width / 2;
+            dataGridViewLiterals.Columns[1].Width = dataGridViewLiterals.Width / 2;
+
+            dataGridViewTokens.Columns.Add("Table", "Таблица");
+            dataGridViewTokens.Columns.Add("Value", "Значение");
+            dataGridViewTokens.Columns[0].Width = dataGridViewTokens.Width / 2;
+            dataGridViewTokens.Columns[1].Width = dataGridViewTokens.Width / 2;
+        }
+
+        private void ButtonLexemeAnalys_Click(object sender, EventArgs e)
+        {
+            ClearDataGridViews();
+            try
+            {
+                LexicalAnalizator analizator = new LexicalAnalizator(textBoxCode.Text);
+                FillDataInTables(analizator);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ClearDataGridViews()
+        {
+            dataGridViewLexeme.Rows.Clear();
+            dataGridViewKeywords.Rows.Clear();
+            dataGridViewVariables.Rows.Clear();
+            dataGridViewSeparators.Rows.Clear();
+            dataGridViewLiterals.Rows.Clear();
+            dataGridViewTokens.Rows.Clear();
+        }
+
+        private void FillDataInTables(LexicalAnalizator analizator)
+        {
+            foreach (var item in analizator.TypesValuesPairs)
+                dataGridViewLexeme.Rows.Add(item.Item1 != "\n" ? item.Item1 : "\\n", item.Item2);
+
+            for (int i = 0; i < analizator.KeyWords.Length; i++)
+                dataGridViewKeywords.Rows.Add(analizator.KeyWords[i], i);
+            for (int i = 0; i < analizator.Separators.Length; i++)
+                dataGridViewSeparators.Rows.Add(analizator.Separators[i], i);
+            for (int i = 0; i < analizator.Variables.Count; i++)
+                dataGridViewVariables.Rows.Add(analizator.Variables[i], i);
+            for (int i = 0; i < analizator.Literals.Count; i++)
+                dataGridViewLiterals.Rows.Add(analizator.Literals[i], i);
+
+            foreach (var item in analizator.tokens)
+                dataGridViewTokens.Rows.Add(item.Item1, item.Item2);
+        }
+
+        private void ButtonOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            else
+            {
+                string filename = openFileDialog.FileName;
+                string fileText = GetTextFromTextFile(filename);
+                textBoxCode.Text = fileText;
+            }
+        }
+
+        private static string GetTextFromTextFile(string path)
+        {
+
+            return System.IO.File.ReadAllText(path);           
+        }
+    }
+    class LexicalAnalizator
+    {
+        private List<(string, string)> typesValuesPairs;
+        List<string> variables = new List<string>();
+        public List<string> Variables
+        {
+            get { return variables; }
+            private set { variables = value; }
+        }
+        public List<string> Literals
+        {
+            get { return literals; }
+            private set { literals = value; }
+        }
+        List<string> literals = new List<string>();
+        public List<(string, int)> tokens = new List<(string, int)>();
+        public List<(string, int)> Tokens
+        {
+            get { return tokens; }
+            private set { tokens = value; }
+        }
+
+        public List<(string, string)> TypesValuesPairs
+        {
+            get { return typesValuesPairs; }
+            private set { typesValuesPairs = value; }
+        }
+        string line;
+
+        readonly string separatorsSingle = "+-*/=,.\n()><!"; 
+        readonly string[] separatorsPairs = { "++", "--", "+=", "-=", "*=", "/=", "==", "!=", ">=", "<=" };
+        public readonly string[] KeyWords = { "Dim", "as", "integer", "double", "float", "for", "to", "next" };
+        string[] separators;
+        public string[] Separators
+        {
+            get { return separators; }
+            private set { separators = value; }
+        }
+
+        
+
+        string state = "S";
+        string buffer = "";
+        public LexicalAnalizator(string line)
+        {
+            this.line = line;
+            typesValuesPairs = new List<(string, string)>();
+            StartAnalys();
+            CompleteAnalys();
+        }
+
+        void CompleteAnalys()
+        {
+            //////////////////////////////////починить костыль
+            string[] tmp = new string[separatorsSingle.Length];            
+            for (int i = 0; i < tmp.Length; i++)
+                tmp[i] = separatorsSingle[i].ToString();
+            //////////////////////////////////////////////////////
+
+            string type = "Unknown";
+            separators = new string[separatorsSingle.Length + separatorsPairs.Length];
+            Array.Copy(tmp, separators, separatorsSingle.Length);
+            Array.Copy(separatorsPairs, 0, separators, separatorsSingle.Length, separatorsPairs.Length);
+            for (int i = 0; i < typesValuesPairs.Count; i++)
+            {
+
+                if (KeyWords.Any(typesValuesPairs[i].Item1.Contains))
+                    type = "Keyword";
+                else if (separators.Any(typesValuesPairs[i].Item1.Contains))
+                    type = "Separator";
+                else if (typesValuesPairs[i].Item2 == "I")
+                    type = "Variable";
+                else if (typesValuesPairs[i].Item2 == "D")
+                    type = "Literal";
+                else
+                    throw new Exception("Неопределенные данные в таблице");
+                type = CreateToken(type, separators, i);
+            }
+        }
+
+        private string CreateToken(string type, string[] separators, int i)
+        {
+            switch (type)
+            {
+                case "Keyword":
+                    for (int j = 0; j < KeyWords.Length; j++)
+                        if (KeyWords[j] == typesValuesPairs[i].Item1)
+                        {
+                            tokens.Add((type, j));
+                            goto default;
+                        }
+                    goto default;
+                case "Separator":
+                    for (int j = 0; j < separators.Length; j++)
+                        if (separators[j] == typesValuesPairs[i].Item1)
+                        {
+                            tokens.Add((type, j));
+                            goto default;
+                        }
+                    goto default;
+                case "Variable":
+                    if (!variables.Contains(typesValuesPairs[i].Item1))
+                        variables.Add(typesValuesPairs[i].Item1);
+                    tokens.Add((type, variables.IndexOf(typesValuesPairs[i].Item1)));
+                    goto default;
+                case "Literal":
+                    if (!literals.Contains(typesValuesPairs[i].Item1))
+                        literals.Add(typesValuesPairs[i].Item1);
+                    tokens.Add((type, literals.IndexOf(typesValuesPairs[i].Item1)));
+                    goto default;
+                default:
+                    type = "Unknown";
+                    break;
+            }
+            return type;
+        }
+
+        void StartAnalys()
+        {
+            state = "S";
+            for (int i = 0; i < line.Length; i++)
+            {
+                switch (state)
+                {
+                    case "S": //состояние неопределенности
+                        if (CheckIsDigit(i)) //проверка на числа
+                        {
+                            state = "D";
+                            goto case "D";
+                        }
+                        else if (CheckIsEnglishAlph(i)) //проверка на английский алфавит
+                        {
+                            state = "I";
+                            goto case "I";
+                        }
+                        else if (CheckIsSingleSeparators(i)) //Проверка на сепараторы
+                        {
+                            state = "R";
+                            goto case "R";
+                        }
+                        else if (CheckIsSpace(i) || CheckIsR(i)) //Проверка на пообел
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            throw new Exception("Неверно задан код");
+                        }
+                    case "R": //состояние сепаратора
+                        Add(i);                      
+                        if (CheckOutOfRange(i + 1) && CheckIsNotSingleSeparators(i+1))
+                        {
+                            i = Next(i);
+                            goto case "R";
+                        }
+                        else
+                        {
+                            Out(buffer, state);
+                            goto default;
+                        }
+                    case "D": //состояние литерала 
+                        Add(i);
+                        if (CheckOutOfRange(i + 1) && (CheckIsDigit(i + 1)))
+                        {
+                            i = Next(i);
+                            goto case "D";
+                        }
+                        else
+                        {
+                            Out(buffer, state);
+                            goto default;
+                        }
+                    case "I": //состояние идентификатора
+                        Add(i);
+                        if (CheckOutOfRange(i + 1) && ((CheckIsDigit(i + 1)) || CheckIsEnglishAlph(i + 1)))
+                        {
+                            i = Next(i);
+                            goto case "I";
+                        }
+                        else
+                        {
+                            Out(buffer, state);
+                            goto default;
+                        }
+                    default:
+                        state = "S";
+                        break;
+                }
+
+            }
+        }
+
+        private static int Next(int i)
+        {
+            return ++i;
+        }
+
+        private bool CheckOutOfRange(int i)
+        {
+            return i < line.Length;
+        }
+
+        private bool CheckIsSpace(int i)
+        {
+            return line[i] == ' ';
+        }
+        private bool CheckIsR(int i)
+        {
+            return line[i] == '\r';
+        }
+        private bool CheckIsDigit(int i)
+        {
+            return Char.IsDigit(line[i]) /*&& (state == "S" || state == "D")*/;
+        }
+
+        private bool CheckIsEnglishAlph(int i)
+        {
+            return ((int)line[i] >= 65 && (int)line[i] <= 90) || ((int)line[i] >= 97 && (int)line[i] <=122);
+        }
+
+        private bool CheckIsSingleSeparators(int i)
+        {
+            return line[i].ToString().Any(letter => separatorsSingle.Contains(letter));
+        }
+        private bool CheckIsNotSingleSeparators(int i)
+        {
+            string tmp = buffer + line[i];
+            bool check = false;
+            foreach (var item in separatorsPairs)         
+                if (item == tmp)
+                    check = true;
+            return check; 
+        }
+
+        private void Add(int i)
+        {
+            buffer += line[i];
+        }
+        private void Out(string buffer, string state)
+        {
+            if (buffer.Length > 0)
+            {
+                typesValuesPairs.Add((buffer, state));
+                this.buffer = "";
+            }
+        }
+    }
+
+}
